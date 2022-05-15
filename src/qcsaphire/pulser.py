09fea12b12394575f084008fad80f7cae9791b68
G Adam Cox <gadamc@gmail.com>
2022-05-15 16:06:13 -0700

    Adds context __enter__ and __exit__ for usage in with statements.
    Adds Property class and methods to make API calls more pythonic.
    That is, instead of
    
    ```
    import qcsaphire
    p = qcsaphire.Pulser('/dev/port')
    p.query(':PULSE0.PERIOD?')
    ```
    
    One can call
    
    ```
    p.pulse0.period()
    ```

--- src/qcsaphire/pulser.py
+++ src/qcsaphire/pulser.py
@@ -24,17 +24,73 @@ def discover_devices():
     return devices
 
 
+class Property( object ):
+    '''
+    Represents a scpi property of the instrument
+    '''
+
+    #--- class methods ---
+
+    def __init__( self, inst, name, arg_separator = ':' ):
+        '''
+        Represents a call to a SCPI instrument's property or method.
+        :param inst: A SCPI instrument resource.
+        :param name: Name of the property.
+            Used to recursively build the property call message.
+        :param arg_separator: Separator to use to separate
+            arguments in a method call.
+            [Default: ':']
+        '''
+        self.__inst = inst  # the instrument
+        self.name = name.upper()
+        self.arg_separator = arg_separator
+
+
+    def __getattr__( self, name ):
+        return Property(
+            self.__inst,
+            ':'.join( ( self.name, name.upper() ) ),
+            arg_separator = self.arg_separator
+        )
+
+
+    def __call__( self, *values ):
+        if len( values ) == 0:
+            # get property
+            return self.__inst.query( f'{self.arg_separator}{ self.name }?')
+
+        else:
+            # set value
+            values = [ str( val ) for val in values ]
+            values = self.arg_separator.join( values )
+
+            cmd = f'{self.arg_separator}{ self.name } { values }'
+            return self.__inst.query( cmd )
+
+
 class Pulser:
 
     def __init__(self, port, timeout=3):
         self._port = port
         self._inst = None
         self._timeout = timeout
+        self.arg_separator = ':'
+        self.last_write_command = None
         self._open()
 
 ## PRIVATE
     def __del__(self):
         self._close()
+
+    def __enter__( self ):
+        return self
+
+    def __exit__( self, exc_type, exc_value, traceback ):
+        self._close()
+
+    def __getattr__( self, name ):
+        resp = Property( self, name, arg_separator = self.arg_separator )
+        return resp
 
     def _open(self):
         if self._inst is not None:
@@ -48,25 +104,30 @@ class Pulser:
 
     def _check_error(self, string):
 
-        if string[0] == '?':
-            if string[1] == '1':
-                raise Exception('Incorrect prefix, i.e. no colon or * to start command.')
-            elif string[1] == '2':
-                raise Exception('Missing command keyword.')
-            elif string[1] == '3':
-                raise Exception('Invalid command keyword.')
-            elif string[1] == '4':
-                raise Exception('Missing parameter.')
-            elif string[1] == '5':
-                raise Exception('Invalid parameter.')
-            elif string[1] == '6':
-                raise Exception('Query only, command needs a question mark.')
-            elif string[1] == '7':
-                raise Exception('Invalid query, command does not have a query form.')
-            elif string[1] == '8':
-                raise Exception('Command unavailable in current system state.')
-            else:
-                raise Exception(f'Unknown Error Indicator {string}')
+        try:
+            if string[0] == '?':
+                if string[1] == '1':
+                    raise Exception(f'Incorrect prefix, i.e. no colon or * to start command. Last write command == {self.last_write_command}')
+                elif string[1] == '2':
+                    raise Exception(f'Missing command keyword. Last write command == {self.last_write_command}')
+                elif string[1] == '3':
+                    raise Exception(f'Invalid command keyword. Last write command == {self.last_write_command}')
+                elif string[1] == '4':
+                    raise Exception(f'Missing parameter. Last write command == {self.last_write_command}')
+                elif string[1] == '5':
+                    raise Exception(f'Invalid parameter. Last write command == {self.last_write_command}')
+                elif string[1] == '6':
+                    raise Exception(f'Query only, command needs a question mark. Last write command == {self.last_write_command}')
+                elif string[1] == '7':
+                    raise Exception(f'Invalid query, command does not have a query form. Last write command == {self.last_write_command}')
+                elif string[1] == '8':
+                    raise Exception(f'Command unavailable in current system state. Last write command == {self.last_write_command}')
+                else:
+                    raise Exception(f'Unknown Error Indicator {string}. Last write command == {self.last_write_command}')
+        except Exception as e:
+            self._readlines() #flush out the read return on error to be ready for next query
+            raise e
+
         return string
 
     def _write(self, data):
@@ -84,7 +145,11 @@ class Pulser:
 
         if not data.endswith(newline_char):
             data += newline_char
+
         self._inst.write(data.encode('utf-8'))
+
+        #we put this below the .write method in case it raises an exception
+        self.last_write_command = data.strip()
 
     def _readline(self):
         '''
@@ -125,7 +190,7 @@ class Pulser:
         if data.upper() in [':INST:COMM?',':INSTRUMENT:COMM?',':INST:COMMANDS?',':INSTRUMENT:COMMANDS?']:
             return_val = self._readlines()
         else:
-            return_val = [self._readline()] #we do this so that this function has a consistent return type
+            return_val = self._readline()
 
         return return_val
 
